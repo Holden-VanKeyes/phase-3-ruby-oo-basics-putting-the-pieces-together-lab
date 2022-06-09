9834bd79e6d09f2a95b36a7b5d7fcf96663af0a6
Abram Shook <ajshook77@gmail.com>
2022-06-08 19:12:49 -0500

    commit tree dd1baa5478752079abbd9f33ac5f41a4b09a17b1

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,27 @@
-# Make your shoe class here!
+class Shoe
+
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+      @brand = brand
+    end
+
+    def properties(brand, color, size, material, condition)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+    end
+
+    def cobble
+
+        @condition = "new"
+        puts "Your shoe is as good as new!"
+
+    
+     
+     
+    end
+
+end
